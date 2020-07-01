421513dd03ddbfd3ba1d0102acea186d9396fee3
ianfuelberth <67341676+ianfuelberth@users.noreply.github.com>
2020-07-01 13:02:26 -0500

    Added storyboards

---
+++ SpaceScreens/Controller/InfoPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoPageViewController.swift
+//  SpaceScreens
+//
+//  Created by Ian Fuelberth on 7/1/20.
+//  Copyright © 2020 Jillian Dlouhy. All rights reserved.
+//
+
+import UIKit
+
+class InfoPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SpaceScreens/Controller/ViewController.swift
+++ SpaceScreens/Controller/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
-    print("Space")
     }
 
 
