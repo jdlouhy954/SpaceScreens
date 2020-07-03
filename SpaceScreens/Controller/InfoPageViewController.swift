dce20b08cfa9d02e05a1773f8d30eaed802497c6
hawk588 <gdhawk588@gmail.com>
2020-07-03 10:20:54 -0500

    info page updates

--- SpaceScreens/Controller/InfoPageViewController.swift
+++ SpaceScreens/Controller/InfoPageViewController.swift
@@ -1,5 +1,5 @@
 //
-//  InfoPageViewController.swift
+//  TablePageViewController.swift
 //  SpaceScreens
 //
 //  Created by Ian Fuelberth on 7/1/20.
@@ -10,8 +10,26 @@ import UIKit
 
 class InfoPageViewController: UIViewController {
 
+    @IBOutlet weak var FunFactLabel: UILabel!
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var DaysLabel: UILabel!
+    @IBOutlet weak var DiameterLabel: UILabel!
+    @IBOutlet weak var DistanceLabel: UILabel!
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    var selectedItem : Planet?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FunFactLabel.text = selectedItem?.funFact
+        NameLabel.text = selectedItem?.title
+        DaysLabel.text = "\(selectedItem?.daysForYear ?? 0)"
+        DiameterLabel.text = "\(selectedItem?.diameter ?? 0)"
+        DistanceLabel.text =  "\(selectedItem?.distanceFromSun ?? 0)"
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
