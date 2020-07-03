fbf0f122f76b4cd470011aa9d67183ae4ee50239
jdlouhy954 <67593839+jdlouhy954@users.noreply.github.com>
2020-07-03 10:53:41 -0500

    Add more planet info

--- Model/Planet.swift
+++ Model/Planet.swift
@@ -28,20 +28,20 @@ public class Planet {
 }
 
 
-let mercury = Planet(title: "Mercury", distanceFromSun: 41973000 , diameter: 10, daysForYear: 10, funFact: "hola")
+let mercury = Planet(title: "Mercury", distanceFromSun: 41973000 , diameter: 4879, daysForYear: 87.97, funFact: "Mercury's surface temperature ranges from -173 to 427 degrees Celsius.")
 
-let venus = Planet(title: "Venus", distanceFromSun: 11, diameter: 11, daysForYear: 11, funFact: "hi")
+let venus = Planet(title: "Venus", distanceFromSun: 67685000, diameter: 12104, daysForYear: 224.70, funFact: "The first accurate observation of Venus was in 1610 by Galileo Galilei.")
 
-let earth = Planet(title: "Earth", distanceFromSun: 12, diameter: 12, daysForYear: 365, funFact: "We live here")
+let earth = Planet(title: "Earth", distanceFromSun: 94508000, diameter: 12742 , daysForYear: 365, funFact: "Around 71% of Earth's surface is covered by water and 29% by land.")
 
-let mars = Planet(title: "Mars", distanceFromSun: 13, diameter: 13, daysForYear: 13, funFact: "adios")
+let mars = Planet(title: "Mars", distanceFromSun: 129800000, diameter: 6805, daysForYear: 686.98, funFact: "Mars has two notable moons, Phobos and Deimos.")
 
-let jupiter = Planet(title: "Jupiter", distanceFromSun: 14, diameter: 14, daysForYear: 14, funFact: "bye")
+let jupiter = Planet(title: "Jupiter", distanceFromSun: 479920000, diameter: 142984, daysForYear: 4332.82, funFact: "Jupiter is the largest planet of the solar system and is twice as massive as all the other planets combined.")
 
-let saturn = Planet(title: "Saturn", distanceFromSun: 15, diameter: 15, daysForYear: 15, funFact: "heyo")
+let saturn = Planet(title: "Saturn", distanceFromSun: 929030000, diameter: 120536, daysForYear: 10756, funFact: "Saturn has the largest planetary rings in the Solar System.")
 
-let uranus = Planet(title: "Uranus", distanceFromSun: 16, diameter: 16, daysForYear: 16, funFact: "hiya")
+let uranus = Planet(title: "Uranus", distanceFromSun: 1840100000, diameter: 51118, daysForYear: 30687.15, funFact: "Twenty-seven moons have been discovered orbiting Uranus.")
 
-let neptune = Planet(title: "Neptune", distanceFromSun: 17, diameter: 17, daysForYear: 17, funFact: "buenas dias")
+let neptune = Planet(title: "Neptune", distanceFromSun: 2781600000, diameter: 49528, daysForYear: 60190.03, funFact: "Neptune is an ice giant and has a “rocky”, icy core.")
 
 
