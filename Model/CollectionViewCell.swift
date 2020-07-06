7a700ee8279a254b7d70c04fdcd19b2c93f42f16
evanriley12 <67593735+evanriley12@users.noreply.github.com>
2020-07-06 00:42:04 -0500

    added collection view

---
+++ Model/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  SpaceScreens
+//
+//  Created by Evan Riley on 7/5/20.
+//  Copyright © 2020 Jillian Dlouhy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+ 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
--- SpaceScreens/Controller/CollectionPageViewController.swift
+++ SpaceScreens/Controller/CollectionPageViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class CollectionPageViewController: UIViewController {
-
+class CollectionPageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+     var selectedPlanet: Planet?
+    
+    let images = [#imageLiteral(resourceName: "Mercury-1"),#imageLiteral(resourceName: "Venus-1"),#imageLiteral(resourceName: "Earth-1"),#imageLiteral(resourceName: "Mars-1"),#imageLiteral(resourceName: "Jupiter-1"),#imageLiteral(resourceName: "Saturn-1"),#imageLiteral(resourceName: "Uranus-1"),#imageLiteral(resourceName: "Neptune-1")]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : CollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.imageView.image = self.images[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedPlanet = planets[indexPath.item]
+        let cell = collectionView.cellForItem(at: indexPath)
+        collectionView.deselectItem(at: indexPath, animated: true)
+        //selectedPlanet = planets[indexPath.item]
+        performSegue(withIdentifier: "toInfoPage", sender: cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let IPVC = segue.destination as! InfoPageViewController
+        IPVC.selectedPlanet = selectedPlanet
+    }
 
     /*
     // MARK: - Navigation
